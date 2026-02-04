e23a470854e6f72b127ca8385cf50895d2e2cf0d
KinjaL1215 <kinjalkharva47@gmail.com>
2026-02-04 23:20:02 +0530

    Add project files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,78 @@
 const express = require('express');
 const path = require('path');
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
 const rootDir = path.join(__dirname, 'views');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(rootDir, 'home.html'));
 });
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(rootDir, 'signup.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(rootDir, 'login.html'));
+});
+
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // check duplicate email
+    const exists = await User.findOne({ email });
+    if (exists) {
+      return res.send("Email already registered ❌");
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await User.create({
+      username,
+      email,
+      password: hash
+    });
+
+    res.send("Signup successful ✅");
+  } catch (err) {
+    console.log(err);
+    res.send("Signup error");
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.send("User not found ❌");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.send("Wrong password ❌");
+    }
+
+    res.send("Login success 🎉");
+
+  } catch (err) {
+    console.log(err);
+    res.send("Login error");
+  }
+});
+
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/wsafe")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log("Mongo Error " ,err));
+
 const PORT = 3000;
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
